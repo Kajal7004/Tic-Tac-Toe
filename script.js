8c1193b2ca03803a8cc8b5ce00b1e67ec1f6febb
Kajal7004 <reshmikjl@gmail.com>
2026-01-18 20:24:48 +0530

    tic-tac-toe

--- script.js
+++ script.js
@@ -7,40 +7,43 @@ let isgameover = false
 
 //function change to turn
 const changeTurn = () => {
-    return turn ="X" ? 0 : "X"
+    return turn === "X"? "0" : "X"
 }
 
 //Function to check for a win
 const checkWin = () => {    
     let boxtext = document.getElementsByClassName("boxtext");
     let wins = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
+      [0, 1, 2, 5, 5, 0],
+      [3, 4, 5, 5, 15, 0],
+      [6, 7, 8, 5, 25, 0],
+      [0, 3, 6,-5, 15, 90],
+      [1, 4, 7, 5, 15, 90],
+      [2, 5, 8, 15, 15, 90],
+      [0, 4, 8, 5, 15, 45],
+      [2, 4, 6, 5, 15, 135],
     ];
     wins.forEach(e => {
         if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && 
         (boxtext[e[2]].innerText === boxtext[e[1]].innerText) &&
-         (boxtext[e[0]].innerText !== " ")){
-            document.querySelector('info').innerText = boxtext[e[0]].innerText + "Won"
+         (boxtext[e[0]].innerText.trim() !== "")){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + "Won";
             isgameover = true;
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = '200px'
-            document.querySelector('.line').style.transform ="translate(23vw,39vw) rotate(90deg)"
+            document.querySelector('.line').style.transform ="translate(${e[3]}vw, ${e[4]}vw) rotate{(e[5]}vw)"
+            document.querySelector('.line').style.width ="20vw";
+
          }
         })
 
     }
 
-
+//Game logic
+// music.play();
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element =>{
     let boxtext = element.querySelector('.boxtext');
-    boxtext.addEventListener('click',() => {
+    element.addEventListener('click',() => {
         if(boxtext.innerText === ''){
             boxtext.innerText = turn;
             turn = changeTurn();
@@ -60,8 +63,9 @@ reset.addEventListener('click' , () =>{
     Array.from(boxtexts).forEach(element => {
         element.innerText=""
     });
-    turn = "X"
+    turn = "X";
     isgameover = false
+    document.querySelector('.line').style.width ="0vw";
     document.getElementsByClassName("info")[0].innerText = "turn for" + turn;
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = '0px'
 
